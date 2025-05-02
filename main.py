91980ed4064e7bdc38c963065b38be556040052d
verlandiya <VeraGonch@yandex.ru>
2025-05-02 14:55:43 +0300

    init

---
+++ main.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+def main():
+    app.run()
+
+
+@app.route('/results/<nickname>/<int:level>/<float:rating>')
+def get_choice(nickname, level, rating):
+    return render_template(
+        'index.html',
+        nickname=nickname,
+        level=level,
+        rating=rating,
+    )
+
+
+if __name__ == '__main__':
+    main()
